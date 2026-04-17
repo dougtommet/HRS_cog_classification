5f32861e3dcd04cdf209aa47d7dfda90ae90411f
dougtommet <doug_tommet@brown.edu>
2026-04-17 16:25:27 -0400

    renamed the files for the first analysis

--- R/A1_000-Control.R
+++ R/A1_000-Control.R
@@ -1,9 +1,9 @@
 
 
-quarto::quarto_render(here::here("R", "000-master.qmd"), output_format = "html")
+quarto::quarto_render(here::here("R", "A1_000-master.qmd"), output_format = "html")
 
 
-fs::file_move(here::here("R", "000-master.html"),
+fs::file_move(here::here("R", "A1_000-master.html"),
               here::here("Reports", stringr::str_c("HRS_cognition_", Sys.Date(),".html")))
 
 
